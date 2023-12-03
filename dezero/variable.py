59b6c8f054ed0189c2bfad811f57fced858934bc
Youngho Oh <42484974+Youngho-Oh@users.noreply.github.com>
2023-12-03 21:43:55 +0900

    [Modified] implements method in variable.py

--- dezero/variable.py
+++ dezero/variable.py
@@ -4,4 +4,7 @@ class Variable :
     def __init__(self, data:float) -> None:
         self.data = data
         self.grad = None
-        
+        self.creator = None
+    
+    def set_creator(self, func) -> None :
+        self.creator = func
