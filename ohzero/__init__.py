698b1156478978263af7c412b4c6d7888ab48781
Youngho Oh <42484974+Youngho-Oh@users.noreply.github.com>
2023-12-22 14:48:36 +0900

    [Update] __init__ python file

--- ohzero/__init__.py
+++ ohzero/__init__.py
@@ -5,4 +5,4 @@ from ohzero.variable import Variable
 # from ohzero.exp import Exp
 # from ohzero.add import Add
 # from ohzero.mul import Mul
-from ohzero.core import Function, Square, Exp, Add, Mul
+from ohzero.core import Function, Neg, Add, Sub, Mul, Div, Square, Exp, Pow
