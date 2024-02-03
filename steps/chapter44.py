2ccbc05c088446e8c318f1c1fa600e83e681cffe
Youngho Oh <42484974+Youngho-Oh@users.noreply.github.com>
2024-02-03 20:53:21 +0900

    [Modified] modified Layer Class

--- steps/chapter44.py
+++ steps/chapter44.py
@@ -10,13 +10,14 @@ np.random.seed(0)
 x = np.random.rand(100, 1)
 y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
 
-l1 = L.Linear(10)
-l2 = L.Linear(1)
+model = L.Layer()
+model.l1 = L.Linear(10)
+model.l2 = L.Linear(1)
 
 def predict(x) :
-    y = l1(x)
+    y = model.l1(x)
     y = sigmoid(y)
-    y = l2(y)
+    y = model.l2(y)
     return y
 
 lr = 0.2
@@ -25,13 +26,13 @@ iters = 10000
 for i in range(iters) :
     y_pred = predict(x)
     loss = mean_squred_error(y, y_pred)
-    l1.cleargrads()
-    l2.cleargrads()
+    # l1.cleargrads()
+    # l2.cleargrads()
+    model.cleargrads()
     loss.backward()
 
-    for l in [l1, l2] :
-        for p in l.params() :
-            p.data -= lr * p.grad.data
+    for p in model.params() :
+        p.data -= lr * p.grad.data
     
     if i % 1000 == 0 :
         print(loss)
